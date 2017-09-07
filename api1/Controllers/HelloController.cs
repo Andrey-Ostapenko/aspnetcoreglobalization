fc680f4cbe54110281d5267098649da135cfda3e
Shabbir Akolawala <sbrakl@gmail.com>
2017-09-07 16:58:21 +0530

    Initial Commit

---
+++ api1/Controllers/HelloController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Resources;
+using System.Reflection;
+using Microsoft.Extensions.Localization;
+
+namespace api1.Controllers
+{
+    [Route("/Hello")]
+    public class HelloController : Controller
+    {
+        private readonly IStringLocalizer<HelloController> _localizer;
+
+        public HelloController(IStringLocalizer<HelloController> localizer)
+        {
+            _localizer = localizer;
+        }
+        
+        // GET api/values
+        [HttpGet]
+        [Route("local")]
+        public string local()
+        {
+            string msg = "";
+            var cul = Thread.CurrentThread.CurrentCulture.Name;
+            var culUI = Thread.CurrentThread.CurrentUICulture.Name;
+            msg = _localizer["Hello"];
+            var lang = Request.Headers["Accept-Language"];
+            string rsFileName = "api1.Resources.rs1";
+            ResourceManager rm = new ResourceManager(rsFileName, Assembly.GetExecutingAssembly());
+            var msg2 = rm.GetString("Hello");
+            var msg3 = rm.GetString("Hello", new System.Globalization.CultureInfo("fr-FR"));
+            msg = msg2;
+            return msg;
+        }
+
+        // GET api/values
+        [HttpGet]
+        [Route("lib")]
+        public string lib()
+        {
+            string msg = "";
+            msg = Lib1.ResRdr.Messenger.GetHello();           
+            return msg;
+        }
+    }
+}
---
+++ api1/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+
+namespace api1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Add locationization
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+         
+
+           
+
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var supportedCultures = new[]
+            {
+                new CultureInfo("en-US"),
+                new CultureInfo("es-ES"),
+                new CultureInfo("fr-FR")
+            };
+
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("en-US"),
+                // Formatting numbers, dates, etc.
+                SupportedCultures = supportedCultures, 
+                // UI strings that we have localized.
+                SupportedUICultures = supportedCultures,
+            });
+
+
+
+            app.UseMvc();
+        }
+    }
+}
